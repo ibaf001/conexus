73a333dab57c77b4400960c7b0c8f9780988b7a9
ibafumba <ibafumba@digonex.com>
2019-12-20 09:00:19 -0500

    Initial part of login page

--- tracker/routes.py
+++ tracker/routes.py
@@ -1,9 +1,12 @@
-from tracker import app, db, bcrypt
-from tracker.models import Employee
 from flask import render_template, url_for, flash, redirect, request, abort
-from tracker.forms import RegistrationForm, LoginForm
-from flask_login import login_user, current_user, logout_user, login_required
+
+from tracker import app,  db, bcrypt
 from tracker import mysql
+from tracker.forms import RegistrationForm, LoginForm
+from tracker.models import Employee, User
+from flask_login import login_user, current_user, logout_user, login_required
+
+
 
 @app.route("/")
 @app.route("/home")
@@ -77,14 +80,43 @@ def upload():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created you are now able to login', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user =User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html', title='Account')
+
