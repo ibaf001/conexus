1cfbf1254919af94672a051ad5dc2589921a2cd2
ibafumba <ibobafumba@gmail.com>
2020-05-04 22:52:44 -0400

    Add logo

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from tracker import create_app
 from tracker.config import ConfigProd
 
-app = create_app()
-#app = create_app(ConfigProd)
+#app = create_app()
+app = create_app(ConfigProd)
 
 if __name__ == '__main__':
-    #app.run(debug=False)
-    app.run(debug=True)
+    app.run(debug=False)
+    #app.run(debug=True)
